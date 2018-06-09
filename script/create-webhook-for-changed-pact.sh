a41246278930408fbd73f45e51cfecc9a3cf56ff
Beth Skurrie <beth@bethesque.com>
2018-06-09 22:55:07 +1000

    chore: update scripts for creating webhooks

--- script/create-webhook-for-changed-pact.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X POST https://test.pact.dius.com.au//webhooks/provider/Animal%20Service/consumer/Zoo%20App -u 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M:O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1' -d@script/webhook-changed-pact.json -H "Content-Type: application/json" -H "Accept: application/hal+json"
---
+++ script/create-webhook-for-verification-published.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST https://test.pact.dius.com.au/webhooks/provider/Animal%20Service/consumer/Zoo%20App -u 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M:O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1' -d@script/webhook-verification-published.json -H "Content-Type: application/json" -H "Accept: application/hal+json"
