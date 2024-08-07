30226c72523ea5cb637778afb33427de2a255dcc
Yousaf Nabi <you@saf.dev>
2024-08-07 22:29:22 +0100

    chore: update broker url to test.pactflow.io

--- script/create-webhook-to-publish-github-status.sh
+++ script/create-webhook-to-publish-github-status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -X POST https://test.pact.dius.com.au/webhooks/provider/Animal%20Service/consumer/Zoo%20App -u 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M:O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1' -d@script/webhook-publish-github-status.json -H "Content-Type: application/json" -H "Accept: application/hal+json"
+curl -X POST https://test.pactflow.io/webhooks/provider/Animal%20Service/consumer/Zoo%20App -u 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M:O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1' -d@script/webhook-publish-github-status.json -H "Content-Type: application/json" -H "Accept: application/hal+json"
 #curl -X POST http://localhost:9292/webhooks/provider/Animal%20Service/consumer/Zoo%20App -d@script/webhook-publish-github-status.json -H "Content-Type: application/json" -H "Accept: application/hal+json"
