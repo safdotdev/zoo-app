a1e67b63a1c8f6270ae2e0fafcfd723e9550c493
Yousaf Nabi <yousafn@gmail.com>
2025-09-07 02:03:13 +0100

    will_respond_with

--- spec/service_providers/animal_service_client_spec.rb
+++ spec/service_providers/animal_service_client_spec.rb
@@ -14,11 +14,11 @@ module ZooApp
         before do
           animal_service.given("there is an alligator named Mary").
             upon_receiving("a request for an alligator").with(
-              method: :get,
-              path: '/alligators/Mary',
+              :get,
+              '/alligators/Mary',
               headers: {'Accept' => 'application/json'} ).
             will_respond_with(
-              status: 200,
+              200,
               headers: {'Content-Type' => 'application/json;charset=utf-8'},
               body: {name: Pact.like('Johnny')}
             )
@@ -35,10 +35,10 @@ module ZooApp
         before do
           animal_service.given("there is not an alligator named Mary").
             upon_receiving("a request for an alligator").with(
-              method: :get,
-              path: '/alligators/Mary',
+              :get,
+              '/alligators/Mary',
               headers: {'Accept' => 'application/json'} ).
-            will_respond_with(status: 404)
+            will_respond_with(404)
         end
 
         it "returns nil" do
@@ -52,11 +52,11 @@ module ZooApp
         before do
           animal_service.given("an error occurs retrieving an alligator").
             upon_receiving("a request for an alligator").with(
-              method: :get,
-              path: '/alligators/Mary',
+              :get,
+              '/alligators/Mary',
               headers: {'Accept' => 'application/json'}).
             will_respond_with(
-              status: 500,
+              500,
               headers: { 'Content-Type' => 'application/json;charset=utf-8'},
               body: {error: 'Argh!!!'})
         end
